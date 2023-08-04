6d250a4925a33af9414bf975ea7fae705e457d21
Shawn Teves <110619604+shWnrict@users.noreply.github.com>
2023-08-04 11:41:50 +0800

    Update verification.php

--- verification.php
+++ verification.php
@@ -1,26 +1,34 @@
-<?php
+<?php require_once('config.php'); ?>
+<style>
+    #uni_modal .modal-content>.modal-footer,
+    #uni_modal .modal-content>.modal-header {
+        display: none;
+    }
+</style>
 
-require_once '../config.php';
+<div class="container-fluid">
+    <form action="" id="registration">
+        <div class="row">
+        <div class="modal fade" id="verificationModal" tabindex="-1" role="dialog" aria-labelledby="verificationModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="verificationModalLabel">Verify Your Email Address</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Please enter the verification code that was sent to your email address.</p>
+        <input type="text" class="form-control" id="verification-code" placeholder="Verification Code">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" id="submit-verification-btn">Verify</button>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
 
-$db = new DBConnection();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $verificationCode = $_POST['verificationCode'];
-
-    $sql = "SELECT * FROM `users` WHERE `verification_code` = '{$verificationCode}'";
-    $result = $db->query($sql);
-
-    if ($result->num_rows === 1) {
-        $row = $result->fetch_assoc();
-
-        $sql = "UPDATE `users` SET `email_verified_at` = CURRENT_TIMESTAMP WHERE `id` = {$row['id']}";
-        $db->query($sql);
-
-        echo json_encode(['status' => 'success']);
-    } else {
-        echo json_encode(['status' => 'failed', 'msg' => 'Invalid verification code.']);
-} else {
-    echo json_encode(['status' => 'failed', 'msg' => 'Invalid request.']);
-}
-
-?>
